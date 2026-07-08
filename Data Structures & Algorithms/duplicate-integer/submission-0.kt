a6d465f1fa3d9408869c44a3f1b3d57411cadd2d
mrJatin <59147317+mrJatin@users.noreply.github.com>
2026-07-08 11:08:57 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        nums.sort()
+        for(i in 1 until nums.size){
+            if(nums[i] == nums[i-1]){
+                return true
+            }
+        }
+        return false
+
+    }
+}
